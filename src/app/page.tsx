f5ceae41133f7b6116480c3abbdbf110a563d314
BlandVoid <sojolhasif@gmail.com>
2023-04-28 19:59:28 +0600

    added social preview link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import styles from "./page.module.scss";
 
+import SocialPreviewButton from "./SocialPreviewButton";
+
 const page = async () => {
     return (
         <main className={styles.container}>
             <h1 className={styles.headline}>React SEO Website</h1>
+            <SocialPreviewButton />
         </main>
     );
 };
